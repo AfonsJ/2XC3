423695bbb326509d74f91901cd6ae9eaac16874e
AfonsJ <javierfabian2003@gmail.com>
2023-02-02 10:29:01 -0500

    added average results for 100 runs on exp 5

--- experiment5.py
+++ experiment5.py
@@ -4,13 +4,14 @@ import timeit
 
 
 alldata = []
+finalresults = []
 # number of runs
 for t in range(100):
     arr = []
     # number of swaps (swap 30 times)
-    for i in range(10,):
-        #list max size 1000, largest num 500
-        l = bad_sorts.create_near_sorted_list(1000, 500, i)
+    for i in range(10,301,10):
+        #list max size 800, largest num 500
+        l = bad_sorts.create_near_sorted_list(800, 500, i)
 
         start = timeit.default_timer()
         good_sorts.quicksort(l)
@@ -20,4 +21,19 @@ for t in range(100):
     alldata.append(arr)
 
 print(len(alldata))
+
+
+# from the 100 runs, get an average for each swap amount
+for m in range(len(alldata[0])):
+    subt = 0
+    for result in alldata:
+        subt += result[m]
+
+
+    finalresults.append(subt/len(alldata))
+
+g=10
+for i in finalresults:
+    print('Average time for '+str(g)+' swaps: '+ str(i))
+    g += 10
 
